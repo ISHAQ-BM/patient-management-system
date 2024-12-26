fa6d71fdc09818f32f6f78a9fb8a838f6bba1122
Yasser-Bachta <yasserbachta01@gmail.com>
2024-12-26 15:30:18 +0100

    GG

--- clientApp/src/app/app.component.ts
+++ clientApp/src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from '../auth/presentation/auth.component';
 import { DashboardComponent } from '../dashboard/presentation/dashboard.component';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-=======
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthRepository } from '../auth/domain/repositories/auth.repository';
-
 @Component({
   selector: 'app-root',
-  template:`<router-outlet></router-outlet>`,
-  imports:[RouterModule]
->>>>>>> 25895233bdc7cb6ab79d6a74182bf7b54cd12b80
+  imports: [RouterOutlet, DashboardComponent , AuthComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
    constructor(private router: Router, private authRepository: AuthRepository) {}
--- clientApp/src/app/app.routes.ts
+++ clientApp/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from '../auth/presentation/auth.component';
-import { DashboardComponent } from '../dashboard/presentation/dashboard.componant';
+import { DashboardComponent } from '../dashboard/presentation/dashboard.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
