b85c8c6de95023e8c0e80fef81b4daa832514be8
ISHAQ-BM <isaacbelhadjmehdi@gmail.com>
2024-12-27 01:07:20 +0100

    add patients-list table component

--- clientApp/src/app/app.routes.ts
+++ clientApp/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from '../auth/presentation/auth.component';
-import { DashboardComponent } from '../dashboard/presentation/dashboard.component';
 import { authGuard } from './auth.guard';
+import { PatientsList } from '../base/presentation/patients-list/patients-list.components';
 
 export const routes: Routes = [
     { path: 'login', component: AuthComponent },
-  { path: 'dashboard',canActivate: [authGuard], component: DashboardComponent },
+  { path: 'dashboard',canActivate: [authGuard], component: PatientsList },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
